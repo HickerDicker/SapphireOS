a01e0d8211446719d241a53da512ebc26d948290
HickerDicker <113861320+HickerDicker@users.noreply.github.com>
2023-10-30 15:13:11 -0700

    update the windows 11 script to add the powerplan and add the powerplan

--- src/PostScript/w11/Script.bat
+++ src/PostScript/w11/Script.bat
@@ -102,6 +102,9 @@ Echo "editing POW & power tweaks"
 powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 powercfg /delete 381b4222-f694-41f0-9685-ff5bb260df2e
 powercfg /delete a1841308-3541-4fab-bc81-f71556f20b4a
+powercfg -import "%WINDIR%\Temp\SapphireOS.pow" 00000000-0000-0000-0000-000000000000
+powercfg /setactive 00000000-0000-0000-0000-000000000000
+powercfg -h off
 powercfg /setacvalueindex scheme_current 2a737441-1930-4402-8d77-b2bebba308a3 d4e98f31-5ffe-4ce1-be31-1b38b384c009 0
 powercfg /setacvalueindex scheme_current 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
 powercfg /setacvalueindex scheme_current 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
